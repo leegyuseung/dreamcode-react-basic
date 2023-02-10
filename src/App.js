71448df85f2d37821e3bae87262272acb610fe91
gyuseung lee <lgs4002@naver.com>
2023-02-10 23:33:38 +0900

    JSX 문법 정리 기본

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "James";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="orange">Hello!</h1>
+      <h2>Hello!</h2>
+      <p>{name}</p>
+      <ul style={{ width: "200px" }}>
+        <li>우유</li>
+        <li>딸기</li>
+        <li>바나나</li>
+      </ul>
+    </>
   );
 }
 

a46337627bbd6d634eefb1c95f668361ca3572de
gyuseung lee <lgs4002@naver.com>
2023-02-10 23:53:05 +0900

    컴포넌트 만들기

--- src/AppJSX.jsx
+++ src/AppJSX.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
-function App() {
+function AppJSX() {
   const name = "James";
   const list = ["우유", "딸기", "바나나", "요거트"];
   return (
@@ -17,4 +17,4 @@ function App() {
   );
 }
 
-export default App;
+export default AppJSX;

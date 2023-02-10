361eea727f55b5182add633de545213cf26088d1
gyuseung lee <lgs4002@naver.com>
2023-02-10 23:40:05 +0900

    JSC 문법 정리 심화

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import "./App.css";
 
 function App() {
   const name = "James";
+  const list = ["우유", "딸기", "바나나", "요거트"];
   return (
     <>
-      <h1 className="orange">Hello!</h1>
+      <h1 className="orange">{`Hello! ${name}`}</h1>
       <h2>Hello!</h2>
       <p>{name}</p>
       <ul style={{ width: "200px" }}>
-        <li>우유</li>
-        <li>딸기</li>
-        <li>바나나</li>
+        {list.map((item) => {
+          return <li>{item}</li>;
+        })}
       </ul>
     </>
   );

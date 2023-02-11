7ea63e5826df2a14ac674baedb1c55cc8cdca02c
gyuseung lee <lgs4002@naver.com>
2023-02-12 00:46:14 +0900

    Props 사용해보기

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -4,7 +4,27 @@ import Profile from "./components/Profile";
 function AppProfile() {
   return (
     <>
-      <Profile />
+      <Profile
+        title="프로트엔드 개발자"
+        name="James"
+        image={
+          "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+        }
+      />
+      <Profile
+        title="백엔드 개발자"
+        name="Anna Young"
+        image={
+          "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80"
+        }
+      />
+      <Profile
+        title="프로트엔드 개발자"
+        name="Bob Yu"
+        image={
+          "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+        }
+      />
     </>
   );
 }

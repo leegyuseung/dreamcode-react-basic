4f5b5906993cdfc09662630f336024454cd7f606
gyuseung lee <lgs4002@naver.com>
2023-02-12 00:53:55 +0900

    새로운 개발자 태그 만들기

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -10,6 +10,7 @@ function AppProfile() {
         image={
           "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
         }
+        isNew="true"
       />
       <Profile
         title="백엔드 개발자"

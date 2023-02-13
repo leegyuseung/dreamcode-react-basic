68c86c9cd5882d496371539409d7017631a3ee4b
gyuseung lee <lgs4002@naver.com>
2023-02-13 22:34:04 +0900

    마우스 따라 가기 개선

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -3,20 +3,22 @@ import { useState } from "react";
 import "./AppXY.css";
 
 export default function AppXY() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-
+  // const [x, setX] = useState(0);
+  // const [y, setY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0 }); // 연관있는 데이터는 객체로 묶어서 관리한다.
   return (
     <div
       className="container"
       onPointerMove={(e) => {
-        setX(e.clientX);
-        setY(e.clientY);
+        setPosition({ x: e.clientX, y: e.clientY });
+        // 만약 수평으로만 이동이 가능하다면?
+        // setPostion(prev => ({x: e.clientX, y: prev.y}))
+        // setPostion(prev => ({...prev, x: e.clientX}))
       }}
     >
       <div
         className="pointer"
-        style={{ transform: `translate(${x}px, ${y}px)` }}
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
       ></div>
     </div>
   );

029d7a34efa8a33bfa807611786ad345f761d178
gyuseung <lgs4002@naver.com>
2023-02-21 09:52:36 +0900

    Form을 만드는 방법

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import AppMentos from "./AppMentos";
+import AppForm from "./AppForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppMentos />
+    <AppForm />
   </React.StrictMode>
 );
 

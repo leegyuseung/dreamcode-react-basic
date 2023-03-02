bd0eca44c713140d713f8c07e1010b3366245e40
gyuseung <lgs4002@naver.com>
2023-03-02 09:25:48 +0900

    로딩,에러 상태 추가

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,22 +1,32 @@
 import React, { useEffect, useState } from "react";
 
 export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(undefined);
+
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
   const handleChange = () => setChecked((prev) => !prev);
   useEffect(() => {
+    setLoading(true);
+    setError(undefined);
     fetch(`data/${checked ? "sale_" : ""}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log("🔥 뜨끈한 데이터를 네트워크에서 받아옴");
         setProducts(data);
-      });
+      })
+      .catch((e) => setError("에러가 발생했음!"))
+      .finally(() => setLoading(false));
     // 컴포넌트가 unmount 될 때 실행되는 return
     return () => {
       console.log("🧹 깨끗하게 청소하는 일들을 합니다.");
     };
   }, [checked]); // checked가 변경될 때마다 useEffect 실행
 
+  if (loading) return <p> Loading...</p>;
+
+  if (error) return <p> {error}</p>;
   return (
     <>
       <input

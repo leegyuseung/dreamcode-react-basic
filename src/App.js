a212dd952cbf3aaf32ff377ba64f3e6ba3b47d34
gyuseung <lgs4002@naver.com>
2023-03-04 23:50:18 +0900

    Tailwind

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import styled, { css } from "styled-components";
 import "./App2.css";
 import Button1 from "./components/Button1";
 import Button2 from "./components/Button2";
-
-const Container = styled.div`
-  display: flex;
-`;
-
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid #3c5b69;
-  color: #b9eaff;
-  margin: 0 1rem;
-  padding: 0.25em 1em;
-  ${(props) =>
-    props.primary &&
-    css`
-      background: #009cd5;
-      color: white;
-    `}
-`;
+import StyledComponent from "./components/StyledComponent";
+import TailwindComponent from "./components/TailwindComponent";
 
 export default function App() {
   return (
     <>
       <Button1 />
       <Button2 />
-      <Container>
-        <Button>Normal Button</Button>
-        <Button primary>Priamry Button</Button>
-      </Container>
+      <StyledComponent />
+      <TailwindComponent />
     </>
   );
 }

db2944280650b18283fc173c69e4e4e5f024deb8
gyuseung <lgs4002@naver.com>
2023-03-04 23:40:17 +0900

    Styled Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import styled, { css } from "styled-components";
 import "./App2.css";
 import Button1 from "./components/Button1";
 import Button2 from "./components/Button2";
 
-function App() {
+const Container = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #3c5b69;
+  color: #b9eaff;
+  margin: 0 1rem;
+  padding: 0.25em 1em;
+  ${(props) =>
+    props.primary &&
+    css`
+      background: #009cd5;
+      color: white;
+    `}
+`;
+
+export default function App() {
   return (
     <>
       <Button1 />
       <Button2 />
+      <Container>
+        <Button>Normal Button</Button>
+        <Button primary>Priamry Button</Button>
+      </Container>
     </>
   );
 }

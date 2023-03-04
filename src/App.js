2cae2e8088fbb7f1b307818a06b4bbb82da725fb
gyuseung <lgs4002@naver.com>
2023-03-04 23:31:03 +0900

    PostCSS 사용

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App2.css";
+import Button1 from "./components/Button1";
+import Button2 from "./components/Button2";
+
+function App() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+    </>
+  );
+}
